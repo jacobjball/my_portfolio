2108a8ba0bcde6dc47277b89bee7ddfa5921d8fc
Jake Ball <jacobjball18@gmail.com>
2020-08-02 14:28:32 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App exends React.Component {
+  super(props);
+  this.state= {
+    title: 'Jake Ball'
+    headerLinks: [
+      { title: 'Home', path: '/'},
+      { title: 'About', path: '/about'},
+      { title: 'Contact', path: '/contact'}
+    ],
+    home: {
+      
+    }
+  }
+
+  render() {
+    return(
+      <div> Hello from React </div>
+    )
+  }
 }
+
 
 export default App;
