aaa6b7d0fe8a9008bccb0c5d7ef8dbb5b32320fc
Jake Ball <jacobjball18@gmail.com>
2020-08-30 23:11:25 -0600

    made responsive

--- src/components/Shared/Home.js
+++ src/components/Shared/Home.js
@@ -11,24 +11,22 @@ const Home = () => (
           I am software engineer that believes hard-work, grit, and consistency bring results. 
       </div>
       <div class="homecontent">
-      <a href='/'>
+        <div class='jacobimage'>
             <img class='jacoboutline' src={JacobOutline}/>
-          </a>
-          <div class='endorsements'>
-        <div class='quote1'>
-          "He is continually persistent in solving and debugging coding problems and all tasks that are put before him." -Henry Doan (DevPoint Labs Instructor & Mentor)<br/><br/>
         </div>
-        <div class='quote2'>
-          "...wants to know how he can be better and when given feedback he's always shown progression..." -Russell Lonczyna (Manager)<br/><br/>
+        <div class='endorsements'>
+          <div class='quote1'>
+            "He is continually persistent in solving and debugging coding problems and all tasks that are put before him." -Henry Doan (DevPoint Labs Instructor & Mentor)<br/><br/>
+          </div>
+          <div class='quote2'>
+            "...wants to know how he can be better and when given feedback he's always shown progression..." -Russell Lonczyna (Manager)<br/><br/>
+          </div>
+          <div class='quote3'>
+            "He is adaptive and flexible, but has a core focus and drive that makes Jake one of our most consistent top performers." -Kelsey Waddell (Sr Manager) <br/>
+          </div>
         </div>
-        <div class='quote3'>
-          "He is adaptive and flexible, but has a core focus and drive that makes Jake one of our most consistent top performers." -Kelsey Waddell (Sr Manager) <br/>
-        </div>
-          
-      </div>
         <br/>
         <br/>
-  
       </div>
     </div>
   </>
